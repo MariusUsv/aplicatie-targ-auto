f68b04ebbc1be67192919622f4d1c7e256bb4bf3
Marius Zaharia Andronic <zaharia.andronic@student.usv.ro>
2026-03-26 21:17:11 +0200

    Add files via upload

---
+++ TargAutoApp/TargAutoApp/AdministrareTranzactiiMemorie.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+public class AdministrareTranzactiiMemorie : IStocareData
+{
+    private List<Tranzactie> lista = new List<Tranzactie>();
+
+    public void AddTranzactie(Tranzactie t)
+    {
+        lista.Add(t);
+    }
+
+    public List<Tranzactie> GetTranzactii()
+    {
+        return lista;
+    }
+}
---
+++ TargAutoApp/TargAutoApp/StocareFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+
+public static class StocareFactory
+{
+    public static IStocareData GetStocare()
+    {
+        string cale = Directory.GetCurrentDirectory() + "\\tranzactii.txt";
+        return new AdministrareTranzactiiFisierText(cale);
+    }
+}
